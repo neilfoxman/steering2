798dc4d599a8046bff5ead1d3de71943d683b702
neil <neilfoxman@gmail.com>
2018-07-18 18:55:23 +0200

    vehicle follows cursor

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+var v;
+var edibles = []
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 360);
+  v = new Vehicle(width / 2, height / 2);
 }
 
 function draw() {
+  background(51);
+
+  v.seek(createVector(mouseX, mouseY));
+  v.update();
+  v.show();
+
 
 }
