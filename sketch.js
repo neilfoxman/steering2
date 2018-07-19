9318a1b10e996249dccd5b8ee9972fb557d9d1d5
neil <neilfoxman@gmail.com>
2018-07-20 00:11:33 +0200

    spawning and surviving, before reproduction

--- sketch.js
+++ sketch.js
@@ -1,33 +1,80 @@
 var v;
 var MARGIN = 50;
+var VEHICLES = [];
 var EDIBLES = [];
 var DEBUGGING = true;
 
 function setup() {
   createCanvas(600, 360);
-  v = new Vehicle(width / 2, height / 2);
 
+  // spawn starting vehicles
+  for (var i = 0; i < 10; i++) {
+    var x = random(MARGIN, width - MARGIN);
+    var y = random(MARGIN, height - MARGIN);
+    var pos = createVector(x, y);
+    VEHICLES.push(new Vehicle(pos));
+  }
+
+  // spawn starting food
   for (var i = 0; i < 10; i++) {
     var x = random(MARGIN, width - MARGIN);
     var y = random(MARGIN, height - MARGIN);
     var pos = createVector(x, y);
     EDIBLES.push(new Edible(pos, EDIBLETYPE.FOOD));
   }
+
+  // spawn starting poison
+  for (var i = 0; i < 5; i++) {
+    var x = random(MARGIN, width - MARGIN);
+    var y = random(MARGIN, height - MARGIN);
+    var pos = createVector(x, y);
+    EDIBLES.push(new Edible(pos, EDIBLETYPE.POISON));
+  }
 }
 
 function draw() {
   background(51);
 
-  v.resetForces();
-  v.seek(EDIBLES);
-  v.applyMargins(MARGIN);
-  v.updateMotion();
-  v.show();
+  // spawn new food
+  if (random() < 0.05) {
+    spawn(EDIBLETYPE.FOOD);
+  }
 
-  for (var i = 0; i < EDIBLES.length; i++) {
-    var edible = EDIBLES[i];
-    edible.show();
+  // spawn new poison
+  if (random() < 0.005) {
+    spawn(EDIBLETYPE.POISON);
   }
 
 
+
+  // Update vehicles and show
+  for (var i = VEHICLES.length - 1; i >= 0; i--) {
+    var v = VEHICLES[i];
+
+    v.reset();
+    v.seek(EDIBLES);
+    if (v.health < 0) {
+      VEHICLES.splice(i, 1);
+    } else {
+      v.applyMargins(MARGIN);
+      v.updateMotion();
+      v.show()
+    };
+
+  }
+
+  // show edibles
+  for (var i = 0; i < EDIBLES.length; i++) {
+    EDIBLES[i].show();
+  }
+}
+
+
+
+
+function spawn(edibleType) {
+  var x = random(MARGIN, width - MARGIN);
+  var y = random(MARGIN, height - MARGIN);
+  var pos = createVector(x, y);
+  EDIBLES.push(new Edible(pos, edibleType))
 }
