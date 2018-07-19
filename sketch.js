ccd7fcf7b0a79ff1cd23da50998bfb312399c836
neil <neilfoxman@gmail.com>
2018-07-19 19:18:53 +0200

    added wander ability when no food around

--- sketch.js
+++ sketch.js
@@ -1,17 +1,33 @@
 var v;
-var edibles = []
+var MARGIN = 50;
+var EDIBLES = [];
+var DEBUGGING = true;
 
 function setup() {
   createCanvas(600, 360);
   v = new Vehicle(width / 2, height / 2);
+
+  for (var i = 0; i < 10; i++) {
+    var x = random(MARGIN, width - MARGIN);
+    var y = random(MARGIN, height - MARGIN);
+    var pos = createVector(x, y);
+    EDIBLES.push(new Edible(pos, EDIBLETYPE.FOOD));
+  }
 }
 
 function draw() {
   background(51);
 
-  v.seek(createVector(mouseX, mouseY));
-  v.update();
+  v.resetForces();
+  v.seek(EDIBLES);
+  v.applyMargins(MARGIN);
+  v.updateMotion();
   v.show();
+
+  for (var i = 0; i < EDIBLES.length; i++) {
+    var edible = EDIBLES[i];
+    edible.show();
+  }
 
 
 }
